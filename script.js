ef7baf3aa4d63af78cb228004b0d084ea82f10c9
DannyBrownWS <167773167+DannyBrownWS@users.noreply.github.com>
2024-06-05 12:32:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-const textInput = document.getElementById("text-input");
-const checkButton = document.getElementById("check-btn");
-const resultContainer = document.getElementById("result-container");
-const resultStatement = document.getElementById("result-statement");
+<!DOCTYPE html>
+<html lang="ena">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./styles.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <title>Palindrome Checker by Danny Brown</title>
+  </head>
+  <body>
+    <main>
+      <h1>Palindrome Checker</h1>
+      <h2>Created by Danny Brown</h2>
 
-const scrubTextInput = () => {
-  const regex = /[\W+\s+_+]/g;
+      <div class="input-container">
+        <div class="inputs">
+          <input type="text" class="text-input" id="text-input" placeholder="Will it palindrome?" />
+          <button id="check-btn">Check</button>
+        </div>
+        <p class="caveat">Note: Punctuation, symbols, and spaces will be ignored.</p>
+      </div>
 
-  const cleanedTextInput = textInput.value.replace(regex, '').toLowerCase();
+      <div class="result-container hide" id="result-container">
+        <p id="result-statement">Sample text goes here</p>
+        
+      </div>
 
-  return cleanedTextInput;
-};
+      <div class="definition-container" id="definition-container">
+        <h4><a class="definition"  href="https://www.merriam-webster.com/dictionary/palindrome#:~:text=pal%C2%B7%E2%80%8Bin%C2%B7%E2%80%8Bdrome,the%20same%20backward%20or%20forward">palindrome</a> <span class="noun">noun</span></h4>
+        <h5>pal·​in·​drome ˈpa-lən-ˌdrōm</h5>
+        <p class="definition-paragraph">: a word, verse, or sentence (such as "Able was I ere I saw Elba") or a number (such as 1881) that reads the same backward or forward</p>
+      </div>
 
-const isPalindrome = () => {
-  
-  if (textInput.value === "") {
-    alert("Please input a value");
-    return;
-  };
-  
-  const cleanedText = scrubTextInput();
-  const splitText = cleanedText.split("");
-  
-  resultStatement.innerHTML = `${textInput.value} <span class="success">is</span> a palindrome`;
-  
-  for (let character = 0; character < splitText.length; character++) {
-    
-    if (splitText[character] !== splitText[splitText.length - 1 - character]) {
-      
-      resultStatement.innerHTML = `${textInput.value} <span class="failure">is not</span> a palindrome`
-      break;
-    }
-  };
-  resultContainer.classList.remove("hide");
-  return resultStatement.innerHTML;
-};
+      <div class="artist">
+        <p>Photo credit: <a class="artist-link" href="https://unsplash.com/@bradleycdunn" >Bradley Dunn</a></p>
+      </div>
 
-checkButton.addEventListener("click", isPalindrome);
+    </main>
 
-textInput.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    isPalindrome();
-  }
-});
+    <script src="./script.js"></script>
+  </body>
+</html>
