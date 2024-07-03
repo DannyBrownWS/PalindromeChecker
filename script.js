cd8e4fc79b918ac6952b9799f63985dd2d9fef8f
DannyBrownWS <167773167+DannyBrownWS@users.noreply.github.com>
2024-07-03 18:51:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
+const restingStar = document.getElementById("resting-div");
+const playingStar = document.getElementById("playing-div");
+const pausedStar = document.getElementById("paused-div");
 const textInput = document.getElementById("text-input");
-const checkButton = document.getElementById("check-btn");
+const checkButton = document.getElementById("check-button");
 const resultContainer = document.getElementById("result-container");
 const resultStatement = document.getElementById("result-statement");
+const clearButton = document.getElementById("clear-button");
+
+let audio = new Audio("https://dbpalindromemp3.s3.us-east-2.amazonaws.com/Serge%2520Quadrado%2520-%2520Autumn%2520Garden.mp3.mp3?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEN7%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMiJGMEQCIA7iKNpXh2d5FhNArM7SD4Vyp8zfZFBaky6HO8XDe6cYAiB4gM395mA0tpT5SGa0CSu2NlyanbZTKDajUh0wXCcFMSrtAgiY%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAAaDDM4MTQ5MTg1MjgxNyIMr24ghJ425Jzx5fn4KsECFgiAp0XW%2Bp0Mo8EohFTTCRdee5ix61Wy0hxnTy7COPJOPD4m1higiz8CAOlehEQLp5WOFCDP4GQ%2BZJPhHCAqdsU%2B78p%2FdZGD269tTgsQGRV7EWtYy55VEJHbdCmlFq73%2Ft3UAinkvbX8wP7BZ4LcpqIQARRm79Op3m%2Fs06VMkJIFpCEAKhx%2FQfDCVm9tw3vGrC%2BjoELcrQ5eF6Tj10cTeDrDdL2iJ%2FSQok0PpIqohiO1ROgzRf6hTnhY3CEcHBBtGHyPK6Qr%2FtEJG7gSfVlKQqkfZ7snlaDKTOrOy68lQJ8dofOaXj73XiJcjgrJr38ITMnPJSrAXGuqs3MDx%2BIwNMHXKFQJ%2B1hoYzsSs40br6n8XGO%2BAqilD8cuQ7F3ZNIK3rTJaXHMt7cCovMk2D9OJpcV5Vkj%2F1C%2Fh29LGWYpRjhzMNyhl7QGOrQC5MgGkLDSMezt6%2BZvvPEysRLcEAUxyOT20zR%2FimAXRuNmkf5gCNDtLljLLYx5KqP05OD1Q1yQBxu29TT2xMwUCA12J8QGQxMsQ419gx1VAhWsIZcuEoDJs0kHkFp1kOWygcZNfn3KfGUgc1%2FJVbM2P7TgAXLh2dRYcyovb82gnj2msSZqdRV3rkNc149h9E%2BZqkAz5Y2lSX%2FOU9XhKuqAYfkNtnuuQx%2Bd0VVOhPf0N7DHB6Tc0M1hsDh5%2BwXgfEXwoCxaPpgjrZ1NooC2Nl9VmGm3F7u2AdghmcRLMJrslwfVAMXFBAPSAZJ%2BXTTlaqNpjusX2YwX%2F7Z3SKCbefkenSOG1mb%2FhdK90orcCz3oUDvQlChGqqCZFbdADjD0YzFgTNpSZv0KoxmUQfB%2F%2B6mUK4g9Z8I%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240703T223350Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAVRUVPWYIXGREBLM5%2F20240703%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Signature=0a874ff43092a908f6793246dbe1132f565eb36d777be7a710a673d8c3166ca6");
+
+const beginMusic = () => {
+  restingStar.classList.add("hide");
+  playingStar.classList.remove("hide");
+  
+  audio.play();
+};
+
+const pauseMusic = () => {
+  playingStar.classList.add("hide");
+  pausedStar.classList.remove("hide");
+  
+  audio.pause();
+};
+
+const playMusic = () => {
+  pausedStar.classList.add("hide");
+  playingStar.classList.remove("hide"); 
+  
+  audio.play();
+};
+
+const musicEnded = () => {
+  playingStar.classList.add("hide");
+  restingStar.classList.remove("hide");
+};
+
+restingStar.addEventListener("click", beginMusic);
+
+playingStar.addEventListener("click", pauseMusic);
+
+pausedStar.addEventListener("click", playMusic);
+
+audio.addEventListener("ended", musicEnded);
 
 const scrubTextInput = () => {
   const regex = /[\W+\s+_+]/g;
@@ -35,6 +75,11 @@ const isPalindrome = () => {
   return resultStatement.innerHTML;
 };
 
+const clearResult = () => {
+  resultContainer.classList.add("hide");
+  textInput.value = "";
+}
+
 checkButton.addEventListener("click", isPalindrome);
 
 textInput.addEventListener("keydown", (event) => {
@@ -42,3 +87,5 @@ textInput.addEventListener("keydown", (event) => {
     isPalindrome();
   }
 });
+
+clearButton.addEventListener("click", clearResult);
